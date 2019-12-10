39118e70121e5502317dc3da18a32f40a9b5ec21
Viktor Mryga <dontworryweareinsafe@gmail.com>
2019-12-10 17:28:40 +0200

    Updates

---
+++ precache-manifest.3fa9ce29215a24d7ce2e77830ebc58ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd61bb28bea5a600d1003af58faaccc7",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "37586658686177363ee1",
+    "url": "/goit-react-hw-04-reader/static/css/main.addd9d42.chunk.css"
+  },
+  {
+    "revision": "83096679c7113a3e7367",
+    "url": "/goit-react-hw-04-reader/static/js/2.4a510459.chunk.js"
+  },
+  {
+    "revision": "37586658686177363ee1",
+    "url": "/goit-react-hw-04-reader/static/js/main.c702d589.chunk.js"
+  },
+  {
+    "revision": "9ebb48dc9c252251feb7",
+    "url": "/goit-react-hw-04-reader/static/js/runtime-main.27418a9c.js"
+  }
+]);
